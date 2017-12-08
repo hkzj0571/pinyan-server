ccbb0ee71f2f1dfcb3a62b6c0164859c07c64c19
Seaony <seaony@outlook.com>
2017-12-08 15:07:22 +0800

    add machine class

--- app/Machines/Machine.php
+++ app/Machines/Machine.php
@@ -2,7 +2,25 @@
 
 namespace App\Machines;
 
+use App\Models\Machine as Model;
+
 class Machine
 {
-    
+    protected $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function touch(array $data, $action)
+    {
+        $params = [
+            'action'    => $action,
+            'user_id'   => auth()->guard('api')->user()->id,
+            'causer_id' => 1,
+        ];
+
+        $this->model->create();
+    }
 }
